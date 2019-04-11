91290e4c8ba108f2466b703ec32e8aeb47555385
Micah Yong <micahtyong@berkeley.edu>
2019-04-11 16:28:04 -0700

    Init repo

---
+++ Enhance/Enhance/ViewControllers/DiagnoseVC.swift
@@ -0,0 +1,18 @@
+//
+//  DiagnoseVC.swift
+//  Enhance
+//
+//  Created by Micah Yong on 4/11/19.
+//  Copyright © 2019 Micah Yong. All rights reserved.
+//
+
+import UIKit
+
+class DiagnoseVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+}
